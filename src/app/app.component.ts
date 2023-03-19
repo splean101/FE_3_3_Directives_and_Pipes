25fde81e783d0bed545d278b8b816f7ee523c3c3
splean101 <101splean@gmail.com>
2023-03-19 16:26:41 +0200

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent  {
+  users = [
+    {name: "Mike", status: "I'm learning angular", url: "https://i.pravatar.cc/150?img=12",},
+    {name: "Nikola", status: "Playing piano", url: "https://i.pravatar.cc/150?img=7",},
+    {name: "Bob", status: "Translations from Chinese", url: "https://i.pravatar.cc/150?img=11", },
+    {name: "Anna ", status: "Pretty girl", url: "https://i.pravatar.cc/150?img=1",},
+];
+
+}
